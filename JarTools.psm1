b053ec2e29e40b721e13ef2fdad9ae3d35a8cdaa
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-05-13 11:04:47 -0500

    add commands * Test-IsGitInstalled * Invoke-GitClone * Edit-JarTools * Get-MyInvocation remove Convert-ImmutableID

--- JarTools.psm1
+++ JarTools.psm1
@@ -66,207 +66,72 @@ function Connect-EC2Instance {
         [string]
         $awsprofile = "default"
     )
-    $instanceID = (Get-EC2Tag -Filter @{Name="tag:Name";Values="$Name"}).ResourceId | Where {$_ -like "i-*"}
+    $instanceID = (Get-EC2Tag -Filter @{Name="tag:Name";Values="$Name"} -profileName $awsprofile).ResourceId | Where {$_ -like "i-*"}
     mstsc /v:localhost:55678
     New-SSMSession -instanceID $instanceID -awsprofile $awsprofile
 }
-function Convert-ImmutableID {
-    <#
 
-        converts decimal value of ms-DS-ConsistencyGuid or ObjectGUID to HEX, ImmutableID, DN and GUID-format.
-
-        Parts of this script is built upon blocks from these scripts:
-        https://identitydude.com/2015/08/01/dn-value-in-aad-sync-aad-connect-the-new-format/
-        https://gallery.technet.microsoft.com/office/Covert-DirSyncMS-Online-5f3563b1
-
-
-    #>
-
-    #Requires -Version 3
-
-    [CmdletBinding()]
-
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$Value
-    )
-
-    # identification helper functions
-
-    function isGUID ($data) { 
-        try { 	$guid = [GUID]$data 
-            return 1 } 
-        catch { return 0 } 
+function Test-IsGitInstalled {
+    try
+    {
+        git | Out-Null
+    return $true
     }
-
-    function isBase64 ($data) { 
-        try { 	$decodedII = [system.convert]::frombase64string($data) 
-                return 1 } 
-        catch { return 0 } 
+    catch [System.Management.Automation.CommandNotFoundException]
+    {
+        return $false
     }
-
-    function isHEX ($data) { 
-        try { 	$decodedHEX = "$data" -split ' ' | foreach-object { if ($_) {[System.Convert]::ToByte($_,16)}}
-            return 1 } 
-        catch { return 0 } 
-    }
-
-    function isDN ($data) { 
-        If ($data.ToLower().StartsWith("cn=")) {
-            return 1 }
-        else {	return 0 }
-    }
-
-    # conversion functions
-
-    function ConvertIItoDecimal ($data) {
-        if (isBase64 $data) {
-            $dec=([system.convert]::FromBase64String("$data") | ForEach-Object ToString) -join ' '
-            return $dec
-        }
-    }
-
-    function ConvertIIToHex ($data) {
-        if (isBase64 $data) {
-            $hex=([system.convert]::FromBase64String("$data") | ForEach-Object ToString X2) -join ' '
-            return $hex
-        }	
-    }
-
-    function ConvertIIToGuid ($data) {
-        if (isBase64 $data) {
-            $guid=[system.convert]::FromBase64String("$data")
-            return [guid]$guid
-        }
-    }
-
-    function ConvertHexToII ($data) {
-        if (isHex $data) {
-            $bytearray="$data" -split ' ' | foreach-object { if ($_) {[System.Convert]::ToByte($_,16)}}
-            $ImmID=[system.convert]::ToBase64String($bytearray)
-            return $ImmID
-        }
-    }
-
-    function ConvertIIToDN ($data) {
-        if (isBase64 $data) {
-            $enc = [system.text.encoding]::utf8
-            $result = $enc.getbytes($data)
-            $dn=$result | foreach { ([convert]::ToString($_,16)) }
-            $dn=$dn -join ''
-            return $dn
-        }
-    }
-
-    function ConvertDNtoII ($data) {
-        if (isDN $data) {
-            $hexstring = $data.replace("CN={","")
-            $hexstring = $hexstring.replace("}","")
-            $array = @{}
-            $array = $hexstring -split "(..)" | ? {$_}
-            $ImmID=$array | FOREACH { [CHAR][BYTE]([CONVERT]::ToInt16($_,16))}
-            $ImmID=$ImmID -join ''
-            return $ImmID
-        }
-    }
-
-    function ConvertGUIDToII ($data) {
-        if (isGUID $data) {
-            $guid = [GUID]$data
-                $bytearray = $guid.tobytearray()
-                $ImmID=[system.convert]::ToBase64String($bytearray)
-            return $ImmID
-        }
-    }
-
-    # from byte string (converted to byte array)
-    If ( ($value -replace ' ','') -match "^[\d\.]+$") {
-        $bytearray=("$value" -split ' ' | foreach-object {[System.Convert]::ToByte($_)})
-        $HEXID=($bytearray| ForEach-Object ToString X2) -join ' '
-
-        $identified="1"
-        Write-host ""
-
-        $ImmID=ConvertHexToII $HEXID
-        $dn=ConvertIIToDN $ImmID
-        $GUIDImmutableID = ConvertIIToGuid $ImmID
-
-        Write-Host "HEX: $HEXID" -foregroundColor Green
-        Write-Host "ImmutableID: $ImmID" -foregroundColor Green
-        Write-Host "DN: CN={$dn}" -foregroundColor Green
-        Write-Host "GUID: $GUIDImmutableID " -foregroundColor Green
-    }
-
-    # from hex
-    If ($value -match " ") {
-        If ( ($value -replace ' ','') -match "^[\d\.]+$") {
-            Return
-        }
-        $identified="1"
-        Write-host ""
-
-        $ImmID=ConvertHexToII $value
-        $dec=ConvertIItoDecimal $ImmID
-        $dn=ConvertIIToDN $ImmID
-        $GUIDImmutableID = ConvertIIToGuid $ImmID
-
-        Write-Host "Decimal: $dec" -foregroundColor Green
-        Write-Host "ImmutableID: $ImmID" -foregroundColor Green
-        Write-Host "DN: CN={$dn}" -foregroundColor Green
-        Write-Host "GUID: $GUIDImmutableID " -foregroundColor Green
-    }
-
-    # from immutableid
-    If ($value.EndsWith("==")) {
-        $identified="1"
-        Write-host ""
-
-        $dn=ConvertIIToDn $Value	
-        $HEXID=ConvertIIToHex $Value
-        $GUIDImmutableID = ConvertIIToGuid $Value
-        $dec=ConvertIItoDecimal $value
-
-        Write-Host "Decimal: $dec" -foregroundColor Green
-        Write-Host "HEX: $HEXID" -foregroundColor Green
-        Write-Host "DN: CN={$dn}" -foregroundColor Green
-        Write-Host "GUID: $GUIDImmutableID " -foregroundColor Green
-    }
-
-    # from  dn
-    If ($value.ToLower().StartsWith("cn=")) {
-        $identified="1"
-        Write-host ""
-
-        $ImmID=ConvertDNToII $Value
-        $HEXID=ConvertIIToHex $ImmID
-        $GUIDImmutableID = ConvertIIToGuid $ImmID
-        $dec=ConvertIItoDecimal $ImmID
-
-        Write-Host "Decimal: $dec" -foregroundColor Green
-        Write-Host "ImmutableID: $ImmID" -foregroundColor Green
-        Write-Host "HEX: $HEXID" -foregroundColor Green
-        Write-Host "GUID: $GUIDImmutableID " -foregroundColor Green
-    }
-
-    # from guid
-    if ( isGuid $Value) {
-        $identified="1"
-        Write-host ""
-
-        $ImmID=ConvertGUIDToII $Value
-        $dn=ConvertIIToDN $ImmID
-        $HEXID=ConvertIIToHex $ImmID
-        $dec=ConvertIItoDecimal $ImmID
-
-        Write-Host "Decimal: $dec" -foregroundColor Green
-        Write-Host "ImmutableID: $ImmID" -foregroundColor Green
-        Write-Host "HEX: $HEXID" -foregroundColor Green
-        Write-Host "DN: CN={$dn}" -foregroundColor Green	
-    }
-
-    If (-not($identified)) {
-        Write-host -fore red "You provided a value that was neither an ImmutableID (ended with ==), a DN (started with CN=), a GUID, a HEX-value nor a Decimal-value, please try again."
-    }
-
-
+    
 }
+
+function Invoke-GitClone {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $url
+    )
+    $gitExt = $url.substring($url.length - 4, 4)
+    if($gitExt -eq ".git"){
+        $splits = $url -Split "/"
+        $repo = $splits[$splits.Count-1].Replace(".git","")
+        if (Test-IsGitInstalled) {
+            git clone $url
+            if (Test-Path "./$repo") {
+                Set-Location "./$repo"
+            }else{
+                Write-Error "Repository was not cloned correctly."
+            }
+        }
+        
+        
+    }else{
+        Write-Error "$url is not a valid git URL"
+    }
+}
+
+function Edit-JarTools {
+    param(
+        [switch]$file
+    )
+    if(!$jartools){
+        $module = Get-Module JarTools
+        $moduleFile = $module.Path
+        $moduleDir = $moduleFile.replace("\$($module.Name).psm1","")
+    }
+    if($file){
+        code $moduleFile
+    }else{
+        code $moduleDir
+    }
+}
+
+function Get-MyInvocation {
+    #$name = $MyInvocation.MyCommand.Name
+    $name = $MyInvocation
+    return $name
+}
+
+Set-Alias -Name igc -Value Invoke-GitClone
+Set-Alias -Name newssm -Value New-SSMSession
+Set-Alias -Name ejt -Value Edit-JarTools
+Export-ModuleMember -Alias * -Function *
